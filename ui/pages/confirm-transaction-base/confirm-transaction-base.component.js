c3629ca8b5f641d175c00a680bda09f493d72f34
Dan Miller <danjm.com@gmail.com>
2021-06-25 07:18:29 -0230

    Allow users to see confirm screen errors.

--- ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
+++ ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
@@ -748,6 +748,7 @@ export default class ConfirmTransactionBase extends Component {
             );
           })
           .catch((error) => {
+            addTxToFailedTxesToDisplay(txData.id);
             this.setState({
               submitting: false,
               submitError: error.message,
