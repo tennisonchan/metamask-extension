3b6b6f6ca0a8a040432dfba594660c26deaff557
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-10-26 04:32:38 +0400

    Fix rebase lint issues Fix bugs in previous commit     Check of status of failedTransaction[id], If it has not failed, we will want to remove it from the list of failed transactions     FIX: failedTransaction is an object not an array
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
+++ ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
@@ -137,7 +137,6 @@ export default class ConfirmTransactionBase extends Component {
     isFailedTransaction: PropTypes.bool,
     removeTxFromFailedTxesToDisplay: PropTypes.func,
     addTxToFailedTxesToDisplay: PropTypes.func,
-    showTransactionErrorDialog: PropTypes.func,
   };
 
   state = {
@@ -699,7 +698,8 @@ export default class ConfirmTransactionBase extends Component {
       maxFeePerGas,
       maxPriorityFeePerGas,
       baseFeePerGas,
-      addTxToDisplay,
+      addTxToFailedTxesToDisplay,
+      removeTxFromFailedTxesToDisplay,
     } = this.props;
     const { submitting } = this.state;
 
@@ -737,7 +737,7 @@ export default class ConfirmTransactionBase extends Component {
         sendTransaction(txData)
           .then(() => {
             removeTxFromFailedTxesToDisplay();
-            clearConfirmTransaction();            
+            clearConfirmTransaction();
             this.setState(
               {
                 submitting: false,
