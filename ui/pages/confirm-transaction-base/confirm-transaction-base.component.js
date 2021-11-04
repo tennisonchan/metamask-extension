127e9b6dac18b51db2a5f1dda7d5afdf2fa26da8
Akintayo A. Olusegun <akintayo.segun@gmail.com>
2021-11-04 17:16:22 +0400

    Remove hasSimulationError from getErrorKey
    Signed-off-by: Akintayo A. Olusegun <akintayo.segun@gmail.com>

--- ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
+++ ui/pages/confirm-transaction-base/confirm-transaction-base.component.js
@@ -233,7 +233,6 @@ export default class ConfirmTransactionBase extends Component {
     if (insufficientBalance) {
       return {
         valid: false,
-        hasSimulationError: false,
         errorKey: INSUFFICIENT_FUNDS_ERROR_KEY,
       };
     }
@@ -241,7 +240,6 @@ export default class ConfirmTransactionBase extends Component {
     if (hexToDecimal(customGas.gasLimit) < Number(MIN_GAS_LIMIT_DEC)) {
       return {
         valid: false,
-        hasSimulationError: false,
         errorKey: GAS_LIMIT_TOO_LOW_ERROR_KEY,
       };
     }
@@ -249,7 +247,6 @@ export default class ConfirmTransactionBase extends Component {
     if (simulationFails) {
       return {
         valid: false,
-        hasSimulationError: true,
         errorKey: simulationFails.errorKey
           ? simulationFails.errorKey
           : TRANSACTION_ERROR_KEY,
@@ -259,14 +256,12 @@ export default class ConfirmTransactionBase extends Component {
     if (noGasPrice && !gasFeeIsCustom) {
       return {
         valid: false,
-        hasSimulationError: false,
         errorKey: GAS_PRICE_FETCH_FAILURE_ERROR_KEY,
       };
     }
 
     return {
       valid: true,
-      hasSimulationError: false,
     };
   }
 
@@ -323,7 +318,7 @@ export default class ConfirmTransactionBase extends Component {
     } = this.props;
     const { t } = this.context;
 
-    const { hasSimulationError } = this.getErrorKey();
+    const hasSimulationError  = Boolean(this.props.txData.simulationFails);
 
     const renderTotalMaxAmount = () => {
       if (
@@ -925,7 +920,8 @@ export default class ConfirmTransactionBase extends Component {
     } = this.state;
 
     const { name } = methodData;
-    const { valid, errorKey, hasSimulationError } = this.getErrorKey();
+    const { valid, errorKey } = this.getErrorKey();
+    const hasSimulationError  = Boolean(txData.simulationFails);
     const {
       totalTx,
       positionOfCurrentTx,
